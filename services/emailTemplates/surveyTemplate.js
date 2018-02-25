1527bc120aadd43945dc2a7188a0069e4f04560c
Sunit Kulkarni <Sunit@Sunits-MBP-2.direcpath.net>
2018-02-25 01:59:12 -0500

    reput previous code placeholder comment

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -6,6 +6,11 @@ openpgp.initWorker({
 
 openpgp.config.aead_protect = true;
 
+var options, encryptedContents;
+var myKey = {};
+
+myKey.pubkey = keys.testPubKey;
+
 module.exports = survey => {
   var fileContents = `
   <html>
@@ -25,18 +30,32 @@ module.exports = survey => {
   </html>
 `;
 
-  var options, encryptedContents;
-  var myKey = {};
-
-  myKey.pubkey = keys.testPubKey;
-
   options = {
     data: fileContents,
     publicKeys: openpgp.key.readArmored(myKey.pubkey).keys
   };
 
-  openpgp.encrypt(options).then(async ciphertext => {
-    encryptedContents = await ciphertext.data;
+  openpgp.encrypt(options).then(ciphertext => {
+    encryptedContents = ciphertext.data;
     console.log(encryptedContents);
+    return encryptedContents;
   });
+
+  //   return `
+  //   <html>
+  //     <body>
+  //       <div style="text-align: center;">
+  //         <h3>I'd like your input!</h3>
+  //         <p>Please answer the following question:</p>
+  //         <p>${survey.body}</p>
+  //         <div>
+  //           <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
+  //         </div>
+  //         <div>
+  //           <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
+  //         </div>
+  //       </div>
+  //     </body>
+  //   </html>
+  // `;
 };
